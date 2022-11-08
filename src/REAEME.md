2ef3d46d7877e7f8d38f2fb457f0c79a0bb024bd
limin <limin176868@gmail.com>
2022-11-08 23:15:12 +0800

    封装DOM库

---
+++ src/REAEME.md
@@ -0,0 +1,6 @@
+# 代码使用办法
+
+```javascript
+yarn global add parcel 或者 npm i -g parcel
+parcel src/index.html
+```
---
+++ src/dom.js
@@ -0,0 +1,35 @@
+
+window.dom = {
+    find(selector, scope) {
+        return (scope || document).querySelectorAll(selector);
+    },
+
+
+    style(node, name, value) {
+        if (arguments.length === 3) {
+            node.style[name] = value
+        } else if (arguments.length === 2) {
+            if (typeof name === "string") {
+                return node.style[name]
+            } else if (name instanceof Object) {
+                const obj = name
+                for (let key in obj) {
+                    node.style[key] = obj[key]
+                }
+            }
+
+        }
+    },
+
+    each(nodeList, fn) {
+        for (let i = 0; i < nodeList.length; i++) {
+            fn.call(null, nodeList[i])
+        }
+
+    }
+
+
+
+
+}
+
